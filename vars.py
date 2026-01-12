eaee83f41e6cbc457ea297368f0b76ff17b7d715
NexusTopha07 <nexusnexus1234x@gmail.com>
2026-01-12 19:17:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,24 +2,24 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "26825556"))
+API_HASH = os.environ.get("API_HASH", "aaabe2cc1fbd66865b6fd99e8f93f1e6")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8274725387:AAF07PJ4A5Q_vX7u1p3hDGNOK3H7r7fPfPU")
 
 CREDIT = os.environ.get("CREDIT", "𝐈𝐓'𝐬𝐆𝐎𝐋𝐔")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "CpprivateApi")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://itsgoluAPI:jrMHSipToKUEnmcp@cpprivateapi.ghhp3oz.mongodb.net/?appName=CpprivateApi")  # Add your own atlas db
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Helcurt_db_user:Sananda14102007@cluster0.ykawida.mongodb.net/?appName=Cluster0")  # Add your own atlas db
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "8429253416"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8429253416").split()]  # Default to owner ID
 
 # Channel Configuration
-PREMIUM_CHANNEL = ""
+PREMIUM_CHANNEL = "-1002520254032"
 # Thumbnail Configuration
-THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "").split())) # Image Link For Default Thumbnail 
+THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "https://i.ibb.co/kVBgGbGG/Picsart-25-10-26-20-52-21-165.jpg").split())) # Image Link For Default Thumbnail 
 
 # Web Server Configuration
 WEB_SERVER = os.environ.get("WEB_SERVER", "False").lower() == "true"
